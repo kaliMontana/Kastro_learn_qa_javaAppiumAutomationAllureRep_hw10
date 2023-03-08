356e4cb3fdaa5c8af310c8947dc967336e306c76
Silver <testingbugsrus@gmail.com>
2023-03-08 15:57:49 +0300

    Added описание окружения to Allure report

--- src/test/java/lib/CoreTestCase.java
+++ src/test/java/lib/CoreTestCase.java
@@ -8,7 +8,9 @@ import org.junit.Before;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.FileOutputStream;
 import java.time.Duration;
+import java.util.Properties;
 
 public class CoreTestCase {
 
@@ -19,6 +21,7 @@ public class CoreTestCase {
 	@Step("Run driver and session")
 	public void setUp() throws Exception {
 		driver = Platform.getInstance().getDriver();
+		this.createAllurePropertyFile();
 		this.rotateScreenPortrait();
 		this.skipWelcomePageForIOSApp();
 		this.openWikiWebPageForMobileWeb();
@@ -75,6 +78,20 @@ public class CoreTestCase {
 			AppiumDriver driver = (AppiumDriver) this.driver;
 			WelcomePageObject welcomePageObject = new WelcomePageObject(driver);
 			welcomePageObject.clickSkip();
+		}
+	}
+
+	private void createAllurePropertyFile() {
+		String path = System.getProperty("allure.results.directory");
+		try {
+			Properties props = new Properties();
+			FileOutputStream fos = new FileOutputStream(path + "/environment.properties");
+			props.setProperty("Environment", Platform.getInstance().getPlatformVar());
+			props.store(fos, "See https://docs.qameta.io/allure/#_environment");
+			fos.close();
+		} catch (Exception e) {
+			System.err.println("IO problem when writing allure properties file ");
+			e.printStackTrace();
 		}
 	}
 }
